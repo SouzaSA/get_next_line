098ea914fceabd6413ddd07de8b61dc785dba77e
sde-alva <sde-alva@student.42sp.org.br>
2021-08-06 12:05:07 -0300

    corrected some bugs locally

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 16:36:56 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/08/06 11:47:24 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/08/06 12:04:46 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ char	*get_next_line(int fd)
 	static t_fd_list	*desc_list = NULL;
 	char				**to_read;
 	char				*str;
+	char				test[1];
 
 	str = NULL;
-	if (fd >= 0 || read(fd, str, 0) == 0)
+	if (fd >= 0 || read(fd, test, 0) == 0)
 	{
 		desc_list = ft_get_fd(&desc_list, fd);
 		if (!desc_list)
