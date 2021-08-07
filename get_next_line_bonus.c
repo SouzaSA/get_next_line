33db95531f54dd604ec0ea64317391f24507ab26
sde-alva <sde-alva@student.42sp.org.br>
2021-08-07 20:39:46 -0300

    removed a bug from ft_get_fd

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,47 +6,45 @@
 /*   By: sde-alva <sde-alva@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 16:36:56 by sde-alva          #+#    #+#             */
-/*   Updated: 2021/08/07 15:06:40 by sde-alva         ###   ########.fr       */
+/*   Updated: 2021/08/07 20:35:41 by sde-alva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-t_fd_list	*ft_get_str(t_fd_list **fd_list, int fd);
+t_fd_list	*ft_get_fd(t_fd_list **fd_list, int fd);
 t_fd_list	*ft_add_fd(t_fd_list **fd_list, int fd);
 void		ft_del_list(t_fd_list **fd_list, int fd);
 
 char	*get_next_line(int fd)
 {
 	static t_fd_list	*desc_list = NULL;
-	char				**to_read;
+	t_fd_list			*fd_node;
 	char				*str;
 
 	str = NULL;
 	if (fd >= 0 && read(fd, str, 0) == 0)
 	{
-		to_read = &(ft_get_str(&desc_list, fd)->str_buff);
-		if (desc_list)
+		fd_node = ft_get_fd(&desc_list, fd);
+		if (fd_node)
 		{
-			to_read = &(desc_list->str_buff);
-			ft_push_line(fd, to_read);
-			if (to_read)
-				str = ft_pop_line(to_read);
+			ft_push_line(fd, &(fd_node->str_buff));
+			str = ft_pop_line(&(fd_node->str_buff));
 		}
 	}
-	if (!str && read(fd, str, 0) == 0)
+	if (!str && fd_node)
 		ft_del_list(&desc_list, fd);
 	return (str);
 }
 
-t_fd_list	*ft_get_str(t_fd_list **fd_list, int fd)
+t_fd_list	*ft_get_fd(t_fd_list **fd_list, int fd)
 {
 	t_fd_list	*list;
 
 	list = *fd_list;
 	while (list && list->fd != fd)
 		list = list->next;
-	if (!list)
+	if (!*fd_list)
 	{
 		*fd_list = ft_add_fd(&list, fd);
 		list = *fd_list;
@@ -89,7 +87,7 @@ void	ft_del_list(t_fd_list **fd_list, int fd)
 	is_clean = 1;
 	if (*fd_list)
 	{
-		node = ft_get_str(fd_list, fd);
+		node = ft_get_fd(fd_list, fd);
 		if (node->str_buff)
 			free(node->str_buff);
 		node->str_buff = NULL;
